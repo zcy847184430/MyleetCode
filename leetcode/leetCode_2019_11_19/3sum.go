5e0c5d6db85cd669d5ee130c6c97307b512decd6
zcy <847184430@qq.com>
2019-11-20 16:56:08 +0800

    3um

--- leetcode/leetCode_2019_11_19/3sum.go
+++ leetcode/leetCode_2019_11_19/3sum.go
@@ -1,4 +1,9 @@
 package leetCode_2019_11_19
+
+import (
+	"sort"
+)
+
 /*
 	给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
 	使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
@@ -23,5 +28,89 @@ func threeSum1(nums []int) [][]int {
 		}
 	}
 
+	return result
+}
+
+func threeSum2(nums []int) [][]int {
+	result := make([][]int, 0)
+	sort.Ints(nums)
+	for i := int(0); i < len(nums) - 2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j := i + 1
+		k := int(len(nums) - 1)
+		for j < k {
+			if j > i + 1 && nums[j] == nums[j-1] {
+				j++
+				continue
+			}
+			if k < len(nums) - 1 && nums[k] == nums[k+1] {
+				k--
+				continue
+			}
+			sum := nums[i] + nums[j] + nums[k]
+			if sum > 0 {
+				k--
+				continue
+			}
+			if sum < 0 {
+				j++
+				continue
+			}
+			if sum == 0 {
+				temp := []int {nums[i], nums[j], nums[k]}
+				result = append(result, temp)
+				k--
+				j++
+				continue
+			}
+		}
+	}
+	return result
+}
+
+func threeSum(nums []int) [][]int {
+	result := make([][]int, 0)
+	sort.Ints(nums)
+	for i := int(0); i < len(nums) - 2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j := i + 1
+		k := int(len(nums) - 1)
+		for j < k {
+			if j > i + 1 && nums[j] == nums[j-1] {
+				j++
+				continue
+			}
+			if k < len(nums) - 1 && nums[k] == nums[k+1] {
+				k--
+				continue
+			}
+			sum := nums[i] + nums[j] + nums[k]
+			if sum > 0 {
+				k--
+				continue
+			}
+			if sum < 0 {
+				j++
+				continue
+			}
+			if sum == 0 {
+				temp := []int {nums[i], nums[j], nums[k]}
+				result = append(result, temp)
+				k--
+				j++
+				continue
+			}
+		}
+	}
 	return result
 }
